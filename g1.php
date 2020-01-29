44073936869d475de973139c12604609c2686a46
jonathonkindle <46705433+jonathonkindle@users.noreply.github.com>
2020-01-28 17:58:18 -0800

    Added HTML
    Added the html boilerplate and moved the php within the body

--- g1.php
+++ g1.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 //group project 1
 if(isset($_POST["convert"])){
     $degree =  $_POST ['degree'];
@@ -59,3 +68,5 @@ if(isset($_POST["convert"])){
         </form>
     ';
 };
+</body>
+</html>
