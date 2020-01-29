94c07397cb1725934a6794961e6029cb6af3acc8
Nice Watthayo <watthayo.warangkana@seattlecentral.edu>
2020-01-29 14:43:29 -0800

    make some changes

--- g1.php
+++ g1.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-// NICE AARON EDIT 2020/01/29
+
+ <?php
 //group project 1
 if(isset($_POST["convert"])){
     $degree =  $_POST ['degree'];
@@ -69,5 +60,4 @@ if(isset($_POST["convert"])){
         </form>
     ';
 };
-</body>
-</html>
+
